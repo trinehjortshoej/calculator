7dcfe06d3753ad65f12fd5b5fcf2d7406610cea1
Trine Hjortshøj <trine@hjortshoej.biz>
2018-08-23 12:58:32 +0200

    Flyttede PHP, så indtastede tal bliver i input

--- index.php
+++ index.php
@@ -13,44 +13,60 @@
 		Felt 1 = indtast tal
 		Felt 2 = knapper med operator
 		Felt 3 = indtast tal
-		Felt 4 = Knap beregn
-		Felt 5 = resultat (ikke skrivbartfelt) -->
-	
-	<form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-		<input type="number" name="val1" required ><br>
-		<input type="number" name="val2" required ><br><br>
-		<button type="submit" name="operator" value="add">+</button>
-		<button type="submit" name="operator" value="sub">-</button>
-		<button type="submit" name="operator" value="mul">*</button><button type="submit" name="operator" value="div">/</button><br><br>
-	</form>
+		Felt 4 = resultat (ikke skrivbartfelt) -->
 	
 	<?php
+	
 	//$v1 = $_GET['val1'];
 	//$v2 = $_GET['val2'];
 	$v1 = filter_input(INPUT_GET, 'val1', FILTER_VALIDATE_INT) or die('missing or illegal val1 parameter');
 	$v2 = filter_input(INPUT_GET, 'val2', FILTER_VALIDATE_INT) or die('missing or illegal val2 parameter');
 	$op = $_GET['operator'];
 		  
-		  echo 'Resultatet er: ';
+		  
 	
 		  switch($op){
 			 case 'add':
 				  $res = $v1+$v2;
+				  $opchar = '+';
 				  break;
 			case 'sub':
 				  $res = $v1-$v2;
+				  $opchar = '-';
 				  break;
 			case 'mul':
 				  $res = $v1*$v2;
+				  $opchar = '*';
 				  break;
 			case 'div':
 				  $res = $v1/$v2;
+				  $opchar = '/';
+				  break;
+			case 'mod':
+				  $res = $v1 % $v2 == 0;
+				  $opchar = '%';
 				  break;
 			  default:
 				  $res = 'Unknown operator "'.$op.'"';
 		  }
 		  
-		  echo $v1.' '.$op.' '.$v2.' = '.$res;
+		  
 	?>
+	
+	<form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+		<input type="number" name="val1" value="<?=$v1?>" required ><br>
+		<input type="number" name="val2" value="<?=$v1?>"2required ><br><br>
+		
+		<button type="submit" name="operator" value="add">+</button>
+		<button type="submit" name="operator" value="sub">-</button>
+		<button type="submit" name="operator" value="mul">*</button><button type="submit" name="operator" value="div">/</button>
+		<button type="submit" name="operator" value="mod">mod</button><br><br>
+	</form>
+	
+	<?php
+	echo 'Resultatet er: ';
+	echo $v1.' '.$opchar.' '.$v2.' = '.$res;
+		  ?>
+
 </body>
 </html>
