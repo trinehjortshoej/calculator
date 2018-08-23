3d17698a0f022199a7e373e95c2944957ec566af
Trine Hjortshøj <trine@hjortshoej.biz>
2018-08-23 13:31:59 +0200

    Skilte PHP efter fejl, hvor default blev indlæst og man ikke kunne indtaste tal

--- index.php
+++ index.php
@@ -16,16 +16,31 @@
 		Felt 4 = resultat (ikke skrivbartfelt) -->
 	
 	<?php
-	//Angiver at det indtastede bliver en værdi
-	//$v1 = $_GET['val1'];
-	//$v2 = $_GET['val2'];
+	//Angiver at det indtastede bliver en værdi og får tallene til at blive efter indtastning, ved hjælp af value i input
+	$v1 = $_GET['val1'];
+	$v2 = $_GET['val2'];
+		  
+	?>
+	
+	<!--Form med input felter og knapper. Form sender til sig selv-->
+	<form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+		<input type="number" name="val1" value="<?=$v1?>" required ><br>
+		<input type="number" name="val2" value="<?=$v1?>"2required ><br><br>
+		
+		<button type="submit" name="operator" value="add">+</button>
+		<button type="submit" name="operator" value="sub">-</button>
+		<button type="submit" name="operator" value="mul">*</button><button type="submit" name="operator" value="div">/</button>
+		<button type="submit" name="operator" value="mod">mod</button><br><br>
+	</form>
+	
+	<?php
 	
 	//Angiver at det indtastede bliver en værdi og validerer for om det er et tal, eller ugyldigt
 	$v1 = filter_input(INPUT_GET, 'val1', FILTER_VALIDATE_INT) or die('missing or illegal val1 parameter');
 	$v2 = filter_input(INPUT_GET, 'val2', FILTER_VALIDATE_INT) or die('missing or illegal val2 parameter');
 	$op = $_GET['operator'];
 	
-		  switch($op){
+	switch($op){
 			 case 'add':
 				  $res = $v1+$v2;
 				  $opchar = '+';
@@ -47,24 +62,8 @@
 				  $opchar = '%';
 				  break;
 			  default:
-				  $res = 'Unknown operator "'.$op.'"';
+				  $res = 'Unknown operator "'.$op.'"';  
 		  }
-	
-		  
-		  
-	?>
-	<!--Form med input felter og knapper. Form sender til sig selv-->
-	<form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-		<input type="number" name="val1" value="<?=$v1?>" required ><br>
-		<input type="number" name="val2" value="<?=$v1?>"2required ><br><br>
-		
-		<button type="submit" name="operator" value="add">+</button>
-		<button type="submit" name="operator" value="sub">-</button>
-		<button type="submit" name="operator" value="mul">*</button><button type="submit" name="operator" value="div">/</button>
-		<button type="submit" name="operator" value="mod">mod</button><br><br>
-	</form>
-	
-	<?php
 	//resultat af indtastning udskrives
 	echo 'Resultatet af '.$v1.' '.$opchar.' '.$v2.' = '.$res;
 		  ?>
